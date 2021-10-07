dd2656078a27aa474f14f73660dc3c4972231a42
AME430 <e0572686@u.nus.edu>
2021-10-07 15:02:16 +0800

    first commit

--- generate_allpitchsamples/generate_allsamples/test.py
+++
@@ -1,8 +0,0 @@
-
-# read song names
-#     song_names = ['S09']
-
-for song_name in song_names:
-    print((singer_name,song_name))
-    if not os.path.exists(ori_path + os.sep + singer_name + os.sep + song_name + '/badsample'):
-        os.makedirs(ori_path + os.sep + singer_name + os.sep + song_name + '/badsample')
